1a1ea7524f286403c654f41a2bf744da8108df8a
Oksana Ross <oksanam999@gmail.com>
2018-04-17 12:29:51 -0700

    First commit

---
+++ MyData.java
@@ -0,0 +1,10 @@
+package myFistProject;
+
+public class MyData {
+	public static void main(String[] args) {
+		System.out.println("My name is Oksana");
+		System.out.println("I am 49 years old");
+		System.out.println("My adress: 1751 Waterfall Dr."+"\n"+"Saint Charles, MO 63303");
+	}
+}
+
