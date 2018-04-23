68ad1ee5a469faed40991a4ad0e2f3d3b366c38c
Oksana999 <38441249+Oksana999@users.noreply.github.com>
2018-04-23 14:32:11 -0700

    Update MyData.java

--- MyData.java
+++ MyData.java
@@ -3,8 +3,8 @@ package myFistProject;
 public class MyData {
 	public static void main(String[] args) {
 		System.out.println("My name is Oksana");
-		System.out.println("I am 49 years old");
-		System.out.println("My adress: 1751 Waterfall Dr."+"\n"+"Saint Charles, MO 63303");
+		System.out.println("I am s years old");
+		System.out.println("My adress: nnn");
 	}
 }
 
